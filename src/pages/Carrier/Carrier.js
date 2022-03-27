a20a5683c9fa050c9c64797ffffed6bdd50d6dc3
tidekung24350 <rps53009@hotmail.com>
2022-03-27 23:57:58 +0700

    fix

--- src/pages/Carrier/Carrier.js
+++ src/pages/Carrier/Carrier.js
@@ -1,60 +1,143 @@
-import React from "react";
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 import "./Carrier.css";
 const Carrier = () => {
+  const [ship, setShip] = useState("");
+  const [shipID, setShipID] = useState("");
+  const [country, setCountry] = useState("");
+  const [status, setStatus] = useState("available");
+  const [shipment, setShipment] = useState("");
+  const [destination, setDestination] = useState("");
+  const [trantime, setTrantime] = useState("");
+  const [date, setDate] = useState("");
+
+  const addCargo = () => {
+    if (
+      ship == "" ||
+      country == "" ||
+      shipment == "" ||
+      destination == "" ||
+      date == ""
+    ) {
+      alert("Please fill full!");
+    } else {
+      axios
+        .post("http://localhost:3001/addship", {
+          ship: ship,
+          shipID: shipID,
+          country: country,
+          status: status,
+          shipment: shipment,
+          destination: destination,
+          trantime: trantime,
+          date: date,
+        })
+        .then((response) => {
+          if(response.data.status === 200){
+            alert(response.data.msg);
+            window.location = "/carrier";
+          }
+        });
+    }
+  };
+
   return (
     <div className="select">
       <header className="app-Header">Add Carrier Ship</header>
       <div className="dataCarrier">
-      <select class="form-select">
-        <option selected>Select Ship</option>
-        <option value="1">All</option>
-        <option value="1">EVERGREEN</option>
-        <option value="2">MAERSK</option>
-        <option value="3">ONE</option>
-        <option value="3">CMA-CGM</option>
-        <option value="3">COSCO</option>
-      </select>
+        <select
+          class="form-select"
+          onChange={(e) => setShip(e.target.value.toLowerCase())}
+        >
+          <option selected value="">
+            Select Ship
+          </option>
+          <option>EVERGREEN</option>
+          <option>MAERSK</option>
+          <option>ONE</option>
+          <option>CMA-CGM</option>
+          <option>COSCO</option>
+        </select>
 
-      <input type="text" id="shipID"  placeholder="Ship ID" class="form-select" />
+        <input
+          required
+          type="text"
+          id="shipID"
+          placeholder="Ship ID"
+          class="form-select"
+          onChange={(e) => setShipID(e.target.value.toLowerCase())}
+        />
 
-      <select class="form-select">
-        <option selected>Select Country</option>
-        <option value="1">Thailand</option>
-      </select>
+        <select
+          class="form-select"
+          onChange={(e) => setCountry(e.target.value.toLowerCase())}
+        >
+          <option selected value="">
+            Select Country
+          </option>
+          <option>Thailand</option>
+        </select>
 
-      <select class="form-select">
-        <option value="1">Available</option>
-        <option value="1">Unavailable</option>
-      </select>
+        <select
+          class="form-select"
+          onChange={(e) => setStatus(e.target.value.toLowerCase())}
+        >
+          <option selected value="available">Available</option>
+          <option value="unavailable">Unavailable</option>
+        </select>
 
-      <select class="form-select">
-        <option selected>Port of Shipment</option>
-        <option value="1">Bangkok Port</option>
-      </select>
+        <select
+          class="form-select"
+          onChange={(e) => setShipment(e.target.value.toLowerCase())}
+        >
+          <option selected value="">
+            Port of Shipment
+          </option>
+          <option>Bangkok Port</option>
+        </select>
 
-      <select class="form-select">
-        <option selected>Port of Destination</option>
-        <option value="1">Bangkok Port</option>
-        <option value="2">Laem Chabang Port</option>
-        <option value="3">Chiang Sean Port</option>
-        <option value="4">Chiang Khong Port</option>
-        <option value="4">Ranong Port</option>
-      </select>
+        <select
+          class="form-select"
+          onChange={(e) => setDestination(e.target.value.toLowerCase())}
+        >
+          <option selected value="">
+            Port of Destination
+          </option>
+          <option>Bangkok Port</option>
+          <option>Laem Chabang Port</option>
+          <option>Chiang Sean Port</option>
+          <option>Chiang Khong Port</option>
+          <option>Ranong Port</option>
+        </select>
 
-      <input type="date" id="calender" class="form-select" />
-      <input type="text" id="logo" class="form-select" placeholder="URL Logo"/>
-    </div>
-    <div className="btnadd">
-    <button className="btn btn-success">Add Carrier</button>
-    </div>
-    <hr style={{width:"50%",marginLeft:"470px"}}/>
-    <div className="btnshow">
-    <NavLink  to="/showcarrier" exact>
-      <button className="btn btn-secondary">Show Carrier</button>
-      </NavLink> 
-    </div>
+        <input
+          required
+          type="number"
+          placeholder="Transection Time (Day)"
+          class="form-select"
+          onChange={(e) => setTrantime(e.target.value + "days")}
+        />
+        <input
+          type="date"
+          id="calender"
+          class="form-select"
+          required
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+      <div className="btnadd">
+        <button className="btn btn-success" onClick={addCargo}>
+          Add Carrier
+        </button>
+      </div>
+      <hr style={{ width: "50%", marginLeft: "470px" }} />
+      <div className="btnshow">
+        <NavLink to="/showcarrier" exact>
+          <button className="btn btn-secondary">Show Carrier</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Popup from "reactjs-popup";
 import "./Order.css";
 const Order = () => {
+  const [history, setHistory] = useState([]);
+  const [status, setStatus] = useState([]);
+
+  const reformatDate = (datetime) =>
+    datetime.getFullYear() +
+    "-" +
+    (datetime.getMonth() + 1) +
+    "-" +
+    datetime.getDate();
+
+  const formatter = new Intl.NumberFormat({
+    style: "currency",
+    currency: "THB",
+  });
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/history").then((response) => {
+      setHistory(response.data.result);
+    });
+  }, []);
+
+  const updateStatus = () => {
+    axios
+      .post("http://localhost:3001/updatestatus", {
+        status:status
+      })
+      .then((response) => {
+        if(response.data.status === 200){
+          alert(response.data.msg);
+          window.location = "/order";
+        }
+      });
+  };
+
   return (
     <div className="container1">
       <div className="headOrder">
@@ -11,219 +47,146 @@ const Order = () => {
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">ORDER ID</th>
-              <th scope="col">DATE/TIME</th>
+              <th scope="col" style={{ marginLeft: "50px" }}>
+                ORDER ID
+              </th>
+              <th scope="col">DATE</th>
+              <th scope="col">TIME</th>
               <th scope="col">STATUS</th>
+              <th scope="col">PRICE</th>
               <th scope="col">ORDER DETAIL</th>
-              <th scope="col">PAYMENT</th>
-              <th scope="col">CHANGE STATUS</th>
-
-              
-              
-              
+              <th scope="col">IMG PAYMENT</th>
+              <th scope="col">STATUS</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>
-                <p style={{ marginTop: "10px" }}>1011000111</p>
-              </th>
-              <td>
-                <p style={{ marginTop: "10px" }}>02/03/2022 13:45 PM</p>
-              </td>
-
-              <td>
-                <p style={{ marginTop: "10px", color: "orange" }}>Pending</p>
-              </td>
-              <td>
-                <p style={{ marginTop: "6px" }}>
-                  <a
-                    class="btn btn-secondary"
-                    style={{ width: "80px", marginLeft: "0px" }}
-                    data-toggle="collapse"
-                    href="#multiCollapseExample1"
-                    role="button"
-                    aria-expanded="false"
-                    aria-controls="multiCollapseExample1"
-                  >
-                    View
-                  </a>
-                </p>
-                <div class="row">
-                  <div class="col">
-                    <div
-                      class="collapse multi-collapse"
-                      id="multiCollapseExample1"
-                    >
-                      <div
-                        class="card card-body"
-                        style={{
-                          border: "1px solid gray",
-                          borderRadius: "15px",
-                          textAlign: "left",
-                        }}
+            {history.length > 0 ? (
+              history.map((h, i) => (
+                <tr>
+                  <th>
+                    <p style={{ marginTop: "10px" }}>{h.bookingId}</p>
+                  </th>
+                  <td>
+                    <p style={{ marginTop: "10px" }}>
+                      {reformatDate(new Date(h.date))}
+                    </p>
+                  </td>
+                  <td>
+                    <p style={{ marginTop: "10px" }}>{h.time}</p>
+                  </td>
+                  <td>
+                    <p style={{ marginTop: "10px", color: "orange" }}>
+                      {h.bookingStatus.toUpperCase()}
+                    </p>
+                  </td>
+                  <td>
+                    <p style={{ marginTop: "10px" }}>
+                      {formatter.format(h.price)} THB
+                    </p>
+                  </td>
+                  <td>
+                    <p style={{ marginTop: "6px" }}>
+                      <a
+                        class="btn btn-secondary"
+                        style={{ width: "80px" }}
+                        data-toggle="collapse"
+                        href={`#multiCollapseExample${i}`}
+                        role="button"
+                        aria-expanded="false"
+                        aria-controls="multiCollapseExample1"
                       >
-                        Service ship : <br />
-                        Country : <br />
-                        Port of Shipment : <br />
-                        Port of Destination : <br />
-                        Container Type : <br />
-                        Container Size : <br />
-                        Container Quantity :
+                        View
+                      </a>
+                    </p>
+                    <div class="row">
+                      <div class="col" style={{ width: "250px" }}>
+                        <div
+                          class="collapse multi-collapse"
+                          id={`multiCollapseExample${i}`}
+                        >
+                          <div
+                            class="card card-body"
+                            style={{
+                              border: "1px solid gray",
+                              borderRadius: "15px",
+                              textAlign: "left",
+                            }}
+                          >
+                            Service ship : {h.serviceName.toUpperCase()}
+                            <br />
+                            Country : {h.country.toUpperCase()}
+                            <br />
+                            Port of Shipment : {h.portShipment.toUpperCase()}
+                            <br />
+                            Port of Destination :{" "}
+                            {h.portDestination.toUpperCase()} <br />
+                            Container Type : {h.containerType.toUpperCase()}
+                            <br />
+                            20 " FT Size Quantity : {h.quantityFT20} <br />
+                            40 " FT Size Quantity : {h.quantityFT40} <br />
+                            45 " FT Size Quantity : {h.quantityFT45} <br />
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </td>
-              <td>
-              <button className="btn btn-primary">View</button>
-              </td>
-              <td>
-                <div className="status">
-                  <select class="form-selectt">
-                    <option selected>Change Status</option>
-                    <option value="1">Pending</option>
-                    <option value="2">Complete</option>
-                    <option value="3">Canceled</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                <p style={{ marginTop: "10px" }}>1011000112</p>
-              </th>
-              <td>
-                <p style={{ marginTop: "10px" }}>15/02/2022 14:25 PM</p>
-              </td>
-              <td>
-                <p style={{ marginTop: "10px", color: "green" }}>Complete</p>
-              </td>
-              <td>
-                <p style={{ marginTop: "6px" }}>
-                  <a
-                    class="btn btn-secondary"
-                    style={{ width: "80px" }}
-                    data-toggle="collapse"
-                    href="#multiCollapseExample2"
-                    role="button"
-                    aria-expanded="false"
-                    aria-controls="multiCollapseExample2"
-                  >
-                    View
-                  </a>
-                </p>
-                <div class="row">
-                  <div class="col">
-                    <div
-                      class="collapse multi-collapse"
-                      id="multiCollapseExample2"
-                    >
-                      <div
-                        class="card card-body"
-                        style={{
-                          border: "1px solid gray",
-                          borderRadius: "15px",
-                          textAlign: "left",
+                  </td>
+                  <td>
+                    {h.bookingStatus === "paid" ? (
+                      <Popup trigger={<a href="#"> View </a>} modal>
+                        <img
+                          src={
+                            "http://localhost:3001/imgpayment/" + h.bookingId
+                          }
+                          width="500"
+                        ></img>
+                      </Popup>
+                    ) : (
+                      <p>{h.bookingStatus}</p>
+                    )}
+                  </td>
+                  <td>
+                    <div className="status">
+                      <select
+                        class="form-selectt"
+                        onChange={(e) => {
+                          let popStatus = status.filter(
+                            (s) => s.bookingId !== h.bookingId
+                          );
+                          let newStatus = [
+                            ...popStatus,
+                            {
+                              bookingId: h.bookingId,
+                              bookingStatus: e.target.value.toLowerCase(),
+                            },
+                          ];
+                          let popId = newStatus.filter(
+                            (n) => n.bookingStatus !== ""
+                          );
+                          setStatus(popId);
                         }}
                       >
-                        Service ship : <br />
-                        Country : <br />
-                        Port of Shipment : <br />
-                        Port of Destination : <br />
-                        Container Type : <br />
-                        Container Size : <br />
-                        Container Quantity :
-                      </div>
+                        <option value="">Change Status</option>
+                        <option>Pending</option>
+                        <option>Complete</option>
+                        <option>Canceled</option>
+                      </select>
                     </div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <button className="btn btn-primary">View</button>
-              </td>
-              <td>
-                <div className="status">
-                  <select class="form-selectt">
-                    <option selected>Change Status</option>
-                    <option value="1">Pending</option>
-                    <option value="2">Complete</option>
-                    <option value="3">Canceled</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                <p style={{ marginTop: "10px" }}>1011000113</p>
-              </th>
-              <td>
-                <p style={{ marginTop: "10px" }}>11/02/2022 12:40 PM</p>
-              </td>
-
-              <td>
-                <p style={{ marginTop: "10px", color: "red" }}>Canceled</p>
-              </td>
-              <td>
-                <p style={{ marginTop: "6px" }}>
-                  <a
-                    class="btn btn-secondary"
-                    style={{ width: "80px" }}
-                    data-toggle="collapse"
-                    href="#multiCollapseExample3"
-                    role="button"
-                    aria-expanded="false"
-                    aria-controls="multiCollapseExample3"
-                  >
-                    View
-                  </a>
-                </p>
-                <div class="row">
-                  <div class="col" style={{ width: "250px" }}>
-                    <div
-                      class="collapse multi-collapse"
-                      id="multiCollapseExample3"
-                    >
-                      <div
-                        class="card card-body"
-                        style={{
-                          border: "1px solid gray",
-                          borderRadius: "15px",
-                          textAlign: "left",
-                        }}
-                      >
-                        Service ship : <br />
-                        Country : <br />
-                        Port of Shipment : <br />
-                        Port of Destination : <br />
-                        Container Type : <br />
-                        Container Size : <br />
-                        Container Quantity :
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td>
-              <button className="btn btn-primary">View</button>
-              </td>
-              <td>
-                <div className="status">
-                  <select class="form-selectt">
-                    <option selected>Change Status</option>
-                    <option value="1">Pending</option>
-                    <option value="2">Complete</option>
-                    <option value="3">Canceled</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <div></div>
+            )}
           </tbody>
         </table>
       </div>
 
       <div className="btnsave">
-        <button style={{ width: "100px" }} className="btn btn-success">
+        <button
+          style={{ width: "100px" }}
+          className="btn btn-success"
+          onClick={updateStatus}
+        >
           Save
         </button>
         <button
